a407749a41ce0e90f4194c237ec1e347661e30d9
root <root@ip-172-31-19-160.us-east-2.compute.internal>
2020-12-01 23:29:07 +0000

    ubuntu

--- blast_test/views.py
+++ blast_test/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import BlastJob, BlastResult
 from .utils import create_query_fasta, extract_sequence
 import os.path
-
+import subprocess
 
 def home(request):
     return render(request, 'main.html')
@@ -16,7 +16,13 @@ def run_blast(request):
     dna_sequence = request.POST.get('dna')
     s = BlastJob(query=dna_sequence)
     s.save()
-    create_query_fasta('./queries/python.fasta', dna_sequence)
+    # create_query_fasta('./queries/python.fasta', dna_sequence)
     # ecoli_seq = extract_sequence()
-    return render(request, 'results.html', {'dna_sequence': dna_sequence})
+    # subprocess.call('/home/ubuntu/TestProject/subprocess_call/docker_run.sh')
+    with open('/home/ubuntu/TestProject/results/sh_aws.out', 'r') as a_file:
+        for line in a_file:
+            x = line.split()
+            s = BlastResult(sstart=x[0], send=x[1], sstrand=x[2], evalue=x[3], pident=x[4], sequence=x[5])
+            s.save()
+    return render(request, 'results.html', {'dna_sequence': dna_sequence}, {'s':s})
 
--- subprocess_call/docker_run.sh
+++ subprocess_call/docker_run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-cd C:/Users/Dibs/Desktop/FullStackTestProject
-
-docker run --rm -v C:/Users/Dibs/Desktop/FullStackTestProject/genome:/blast/genome:ro -v C:/Users/Dibs/Desktop/FullStackTestProject/queries:/blast/queries:ro -v C:/Users/Dibs/Desktop/FullStackTestProject/results:/blast/results:rw ncbi/blast blastn -query /blast/queries/ecoli_k12_mg1655.fasta -subject /blast/genome/ecoli_k12_mg1655.fasta -out /blast/results/shell.outfmt6 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
+cd ~/TestProject
+chmod +x docker_run.sh
+sudo docker run --rm \
+	 -v ~/TestProject/genome:/blast/genome:ro \
+	 -v ~/TestProject/queries:/blast/queries:ro \
+	 -v ~/TestProject/results:/blast/results:rw \
+	 ncbi/blast \
+	 blastn -query /blast/queries/ecoli_k12_mg1655.fasta -subject /blast/genome/ecoli_k12_mg1655.fasta \
+        -out /blast/results/sh_aws.out -outfmt "6 sstart send sstrand evalue pident sseq" 
